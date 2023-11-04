18d9484f1fb9f74557db8eee95abcec26ae1304c
Yusuf Ali <yusufa9119@gmail.com>
2023-11-04 11:44:43 +0000

    Commited

---
+++ Client.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args) {
+        final String serverHost = "localhost";  // Change to the server's IP or hostname
+        final int serverPort = 12345;
+
+        try (Socket socket = new Socket(serverHost, serverPort)) {
+            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+
+            // You can send and receive data to/from the server here
+
+            // Example: Receiving a message from the server
+            String message = (String) in.readObject();
+            System.out.println("Server: " + message);
+
+            // Cleanup
+            in.close();
+            out.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
+    
+}
---
+++ Server.java
@@ -0,0 +1,57 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class Server {
+
+    public static void main(String[] args) {
+        final int port = 12345;
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Server is listening on port " + port);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Client connected: " + clientSocket);
+
+                // Create a new thread to handle each client connection
+                Thread clientThread = new Thread(new ClientHandler(clientSocket));
+                clientThread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class ClientHandler implements Runnable {
+    private Socket clientSocket;
+
+    public ClientHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+            ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
+
+            // You can now send and receive data from the client here
+            // Implement your code to handle CO2 concentration, postcode, timestamp, and date
+
+            // Example: Sending a message to the client
+            out.writeObject("Hello, client! This is the server.");
+
+            // Cleanup
+            in.close();
+            out.close();
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
